d572bc15e8adda20f5e7a582e6a94855a98b4d26
Zohaib <zohaib.codemaster@gmail.com>
2026-07-15 15:46:59 +0500

    read message from yaml and display through ConfigurationProperties annotation

---
+++ src/main/java/com/training/lecture02/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.training.lecture02;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ConfigurationProperties(prefix="initial")
+public class WelcomeController {
+    private String message;
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    @GetMapping("/api/v1/welcome")
+    public String welcome() {
+        return message;
+    }
+}
--- src/main/java/com/training/lecture02/config/ApiSecurityConfiguration.java
+++ src/main/java/com/training/lecture02/config/ApiSecurityConfiguration.java
@@ -4,12 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,24 +21,5 @@ public class ApiSecurityConfiguration {
                 .csrf(config -> config.disable());
 
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails user = User.builder()
-                .username("reporter").password(passwordEncoder.encode("password")).roles("REPORTER").build();
-
-        UserDetails editor = User.builder()
-                .username("editor").password(passwordEncoder.encode("password")).roles("EDITOR").build();
-
-        UserDetails admin = User.builder()
-                .username("admin").password(passwordEncoder.encode("admin123")).roles("ADMIN").build();
-
-        return new InMemoryUserDetailsManager(user, editor, admin);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
