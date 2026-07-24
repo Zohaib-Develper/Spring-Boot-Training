bd34c9c77d314f6e7ad7907d573be86e8fda7100
Zohaib <zohaib.codemaster@gmail.com>
2026-07-24 15:49:43 +0500

    Use query annotation

--- src/main/java/com/training/lecture02/users/ApiUserRepository.java
+++ src/main/java/com/training/lecture02/users/ApiUserRepository.java
@@ -2,8 +2,11 @@ package com.training.lecture02.users;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ApiUserRepository extends JpaRepository<ApiUser, Long> {
 
-  Optional<ApiUser> findByUsername(String username);
+  @Query("SELECT u FROM ApiUser u WHERE u.username = :username")
+  Optional<ApiUser> findByUsername(@Param("username") String username);
 }
